75718a110805f4459ee672243c89436f08374b27
Michael Akinwole <michaeltsola22@gmail.com>
2022-10-13 15:25:00 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 /**
-
-* main - Entry point
- 
-* Return: Always 0 
-*/
-
-int main(void)
-
+ * main - Entry
+ * Return; Always 0
+ */
 {
-
-	puts( "Programming is like building a multilingual puzzle");   
-       	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
