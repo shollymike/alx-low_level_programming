a66594edafbb71b878e219ef642c4b452e73d9b6
Michael Akinwole <michaeltsola22@gmail.com>
2022-10-18 19:01:26 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main"
 
 /**
- * times_table - prints the 9 times table
+ *  times_table - Prints the 9 times table, starting with 0.
  */
 void times_table(void)
 {
@@ -18,12 +18,14 @@ void times_table(void)
 
 			prod = num * mult;
 
-			if (prod <= 0)
+			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
+
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
 }
+
